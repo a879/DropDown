9e6a962592845def1cf36d851bcdc8dfdb2c92f8
Dzzz <963351665@qq.com>
2023-12-11 04:18:03 +0800

    添加下拉菜单组件

---
+++ DropDown.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-dropdown>
+      <el-button type="primary">
+        Dropdown List<el-icon class="el-icon--right"><arrow-down /></el-icon>
+      </el-button>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>Action 1</el-dropdown-item>
+          <el-dropdown-item disabled>Action 2</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+</template>
+
+<script lang='ts'>
+
+export default {
+    name: '',
+    setup() {
+
+        return { }
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
